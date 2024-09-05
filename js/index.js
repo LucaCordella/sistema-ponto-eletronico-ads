ced151f6d72c6686bc645d2241b6031d2a1a6e97
LucaCordella <lucacordella12@gmail.com>
2024-09-05 08:41:07 -0300

    alteracao 5 JS - dayWeek

--- js/index.js
+++ js/index.js
@@ -1,9 +1,12 @@
 const diaSemana = document .getElementById("dia-semana");
 const diaMesAno = document .getElementById("dia-mes-ano");
 const horaMinSeg = document .getElementById("hora-min-seg"); 
+const arrayDayWeek = ["Domingo", "Segunda-Feira", "Terça-Feira", "Quarta-Feira", "Quinta-Feira", "Sexta-Feira", "Sábado"]
 
-// Todo conjunto numérico (exceto ano) deve ter 2 dígitos (adicioanar 0 se for menor que 10)
-// Retornar dia da semana por extenso (em pt-br)
+function daySemana() {
+    const date = new Date();
+    return arrayDayWeek[date.getDay()]
+}
 
 function dataCompleta() {
     const date = new Date();
@@ -15,5 +18,6 @@ function horaCompleta() {
     return date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
 }
 
+diaSemana.textContent = daySemana();
 diaMesAno.textContent = dataCompleta();
 horaMinSeg.textContent = horaCompleta();
